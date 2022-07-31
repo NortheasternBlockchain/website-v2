a8d51bee5aab2a0f789e29c455ab3c5715a82b14
jarenadams21 <jarenadams3@gmail.com>
2022-07-31 17:06:21 -0400

    grid experiments

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,11 +1,22 @@
 import React from 'react'; // required for Gatsby
 import { AppLayout } from '../layouts/AppLayout';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Team() {
     return (
         <AppLayout>
             <h2>Team</h2>
-            <h2>Test</h2>
+            <Container>
+                <Row>
+                    <Col md={3} sm={6} xs={6}>1st</Col>
+                    <Col md={3} sm={6} xs={6}>2nd</Col>
+                    <Col md={3} sm={6} xs={6}>3rd</Col>
+                    <Col md={3}s sm={6} xs={6}>4th</Col>
+                </Row>
+                </Container>
         </AppLayout>
     );
 }
