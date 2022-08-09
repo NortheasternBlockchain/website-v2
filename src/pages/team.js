ac121295fa284140612a3fbec41b3a8d8d16e8a9
jarenadams21 <jarenadams3@gmail.com>
2022-08-09 13:55:55 -0400

    imported headshots / old team json

--- src/pages/team.js
+++ src/pages/team.js
@@ -8,68 +8,134 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Nishay from "../images/headshots/nsh.jpeg";
+import Ben from "../images/headshots/ben.jpeg";
+import Ivan from "../images/headshots/ivan.jpeg";
+import Shubh from "../images/headshots/shubh.jpeg";
+import Youssef from "../images/headshots/Youssef.jpeg";
+import Vasu from "../images/headshots/vasu.jpeg";
+import Nora from "../images/headshots/nora.jpeg";
+import Megan from "../images/headshots/meg.jpeg";
+import Alyson from "../images/headshots/ally.jpeg";
+import Jeremy from "../images/headshots/jeremy.jpeg";
+import David from "../images/headshots/david.jpeg";
+import Bernice from "../images/headshots/bernice.jpeg";
+import Luke from "../images/headshots/luke.jpeg";
 
-const cardInfo = [{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-},
-{
-    "name": "Nishay Madhani",
-    "title": "Director of Technology",
-    "img": "./src/images/profile.jpg",
-    "social": "https://nshmadhani.com"
-}];
+const cardInfo = [
+  {
+    description:
+      " Alyson is doing Master’s in Business Analytics (MSBA’22). She loves Music, food, and clean. Her favorite crypto is Cosmos",
+    title: "Alyson (Ran) Liu (Co-President)",
+    image: Alyson,
+    link: "https://www.linkedin.com/in/alysonliu/",
+    id: 1,
+  },
+  {
+    description:
+      "David is doing Master's in Computer Science and he loves Video Games, Yield Farming, and Snowboarding. His favorite crypto is Bitcoin",
+    title: "David Hsu (Co-President)",
+    image: David,
+    link: "https://www.linkedin.com/in/david-hsu-cfa-13858542/",
+    id: 2,
+  },
+  {
+    description:
+      "Bennett is a 2nd Year, Data Science and FinTech Major working on his startup called BlockBeam. His favorite crypto is Algorand",
+    title: "Bennett Thompson (Co Vice President)",
+    image: Ben,
+    link: "https://www.linkedin.com/in/thompsonbennett/",
+    id: 3,
+  },
+  {
+    description: "",
+    title: "Jeremy Koch (Co Vice President)",
+    image: Jeremy,
+    link: "https://www.linkedin.com/in/jeremy-kochnu/",
+    id: 4,
+  },
+  {
+    description:
+      "Noura is a 2nd year student studying Finance and Experience Design. She is into MMA, Fencing, Origami, and Travelling. Her favorite crypto is Ethereum",
+    title: "Noura Abdalla (Co-Director of Events)",
+    image: Nora,
+    link: "https://www.linkedin.com/in/noura-a/",
+    id: 14,
+  },
+  {
+    description:
+      "Youssef is a 4th Year student majoring in Finance and Corporate Innovation & Ventures. He is interested in NFTs, Art, Fashion, DeFi, and Layer 2s. His favorite crypto is Ethereum",
+    title: "Youssef Haidar (Co-Director of Research)",
+    image: Youssef,
+    link: "https://www.linkedin.com/in/youssefhh/",
+    id: 6,
+  },
+  {
+    description:
+      "Vasu is a 3rd year student studying CS & Finance. He loves Climbing and economics. His favorite crypto is Ethereum",
+    title: "Vasu Zalawadia (Co-Director of Research)",
+    image: Vasu,
+    link: "https://www.linkedin.com/in/vzalawadia/",
+    id: 7,
+  },
+  {
+    description: "",
+    title: "Nishay Madhani (Director of Technology)",
+    image: Nishay,
+    link: "https://nshmadhani.com",
+    id: 8,
+  },
+  {
+    description:
+      "Ivan is a 2nd year Computer Science & Mathematics major. He is into Soccer and DeFi. His favorite crypto is Ethereum.",
+    title: "Ivan Rosales (Director of Governance)",
+    image: Ivan,
+    link: "https://www.linkedin.com/in/ivangrosales/",
+    id: 9,
+  },
+  {
+    description: "",
+    title: "Bernice Luong (Director of Marketing)",
+    image: Bernice,
+    link: "https://www.linkedin.com/in/bernice-luong/",
+    id: 10,
+  },
+  {
+    description: "",
+    title: "Megan Gilbert (Head of Social Media)",
+    image: Megan,
+    link: "https://www.linkedin.com/in/megan--gilbert/",
+    id: 11,
+  },
+  {
+    title: "Shubh Patni (Head of Public Relations)",
+    image: Shubh,
+    description:
+      "Shubh Patni is a first year student studying CS and Economics. He is a Blogger, Programmer, and creates content related to programming and tech. He is working at Phemex Student Fellowship and Coinbase Analyst Program. His Favorite Crypto is Ethereum",
+    link: "https://shubhpatni.com",
+    id: 12,
+  },
+  {
+    description: "",
+    title: "Luke O'Neill (Director of Operations)",
+    image: Luke,
+    link: "https://www.linkedin.com/in/lukefoneill",
+    id: 13,
+  },
+];
 
 
 const renderCard = (card, index) => {
 
     return(
         <Card style={{ width: '18rem' }} text="dark" key={index} class="box">
-      <Card.Img variant="top" src="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg" />
+      <Card.Img variant="top" src= {card.image} />
       <Card.Body>
-        <Card.Title>{card.name}</Card.Title>
+        <Card.Title>{card.title}</Card.Title>
         <Card.Text>
-          {card.title}
+          {card.description}
         </Card.Text>
-        <Button href={card.social} variant="primary">Social</Button>
+        <Button href={card.link} variant="primary">Social</Button>
       </Card.Body>
     </Card>
     )
@@ -79,9 +145,14 @@ function Team() {
     return (
         <AppLayout>
             <h2>Team</h2>
-            <div className="grid">
-            {cardInfo.map(renderCard)}
-            </div>
+            <Container>
+                <Row>
+                    <Col md={3} sm={6} xs={6}>1st</Col>
+                    <Col md={3} sm={6} xs={6}>2nd</Col>
+                    <Col md={3} sm={6} xs={6}>3rd</Col>
+                    <Col md={3}s sm={6} xs={6}>4th</Col>
+                </Row>
+                </Container>
         </AppLayout>
     );
 }
@@ -99,3 +170,9 @@ export default Team;
                 </Row>
                 </Container>
 */
+
+/*
+<div className="grid">
+            {cardInfo.map(renderCard)}
+            </div>
+            */
